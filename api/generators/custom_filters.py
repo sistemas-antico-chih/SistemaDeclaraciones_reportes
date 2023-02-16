7f7feee0e09cc1470321151e652a200ff802f7e6
= <=>
2023-02-16 08:57:49 -0600

    obteniendo nombre ente publico

--- api/generators/custom_filters.py
+++ api/generators/custom_filters.py
@@ -52,7 +52,7 @@ def replacePipe(value: str, strToReplace: str, replacementStr: str):
       return value
     return value.replace(strToReplace, replacementStr)   
 
-def replaceInstitutionPipe(value: str, institutionKey: str, type: str):
+def replaceInstitutionPipe(value: str, institutionKey = "MXN ": str , type = "MUN_CHIH": str):
     if (value):
         with open('assets/json/instituciones.json') as file:
             data = json.load(file)
