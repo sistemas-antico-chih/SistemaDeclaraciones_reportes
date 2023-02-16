3ab835d1707610946dbfe5285483651da4c36ccb
= <=>
2023-02-16 08:59:10 -0600

    obteniendo nombre ente publico

--- api/generators/custom_filters.py
+++ api/generators/custom_filters.py
@@ -52,7 +52,7 @@ def replacePipe(value: str, strToReplace: str, replacementStr: str):
       return value
     return value.replace(strToReplace, replacementStr)   
 
-def replaceInstitutionPipe(value: str, institutionKey = "MXN ": str , type = "MUN_CHIH": str):
+def replaceInstitutionPipe(value: str, institutionKey = "MXN", type = "MUN_CHIH": str):
     if (value):
         with open('assets/json/instituciones.json') as file:
             data = json.load(file)
