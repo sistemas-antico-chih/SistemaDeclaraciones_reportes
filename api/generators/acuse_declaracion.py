c6d86649776c801e41aa0374d2678033e9840f5a
= <=>
2025-07-17 14:33:44 -0600

    aviso

--- api/generators/acuse_declaracion.py
+++ api/generators/acuse_declaracion.py
@@ -22,18 +22,18 @@ from api.generators.custom_filters import dateTranslationPipe
 
 
 class AcuseDeclaracionGenerator(object):
-    def __init__(self, owner: str, institucionData:Dict[str, Any], id: str, data: Dict[str, Any], preliminar: bool = False, publico: bool = False):        
+    def __init__(self, owner: str, institucionData:Dict[str, Any], id: str, data: Dict[str, Any], preliminar: bool = False, publico: bool = False):
         # print(json.dumps(institucionData,indent=2))
         self.owner:str = owner
         self.institucionData: Dict[str,Any] = institucionData
-        self.id: str = id    
+        self.id: str = id
         self.data: Dict[str, Any] = data
         self.preliminar = preliminar
         self.publico = publico
 
-    def addJson(self):        
+    def addJson(self):
         self.data.update(self.institucionData)
-        
+
 
     def make_pdf(self):
         env: Environment = Environment(loader=FileSystemLoader('.'))
